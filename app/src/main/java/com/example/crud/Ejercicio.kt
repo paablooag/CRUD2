fdb1183e7168be92713f14501d1ea7ce8559defa
EAG <pablo.alcalde.garcia@gmail.com>
2023-12-18 17:57:08 +0100

    commit

--- app/src/main/java/com/example/crud/Ejercicio.kt
+++ app/src/main/java/com/example/crud/Ejercicio.kt
@@ -2,6 +2,8 @@ package com.example.crud
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 
 @Parcelize
 data class Ejercicio(
@@ -10,33 +12,4 @@ data class Ejercicio(
     var repeticiones: Int? = null,
     var series: Int? = null,
     var imagen: String? = null
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readValue(Int::class.java.classLoader) as? Int,
-        parcel.readString()
-    ) {
-    }
-
-    override fun describeContents(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun writeToParcel(p0: Parcel, p1: Int) {
-        TODO("Not yet implemented")
-    }
-
-    companion object CREATOR : Parcelable.Creator<Ejercicio> {
-        override fun createFromParcel(parcel: Parcel): Ejercicio {
-            return Ejercicio(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Ejercicio?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
-
-annotation class Parcelize
+) : Parcelable
